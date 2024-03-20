b39653027a83b0852cdaf54dcde1381dc87b996d
Belgnaoui Othman <belgnaoui.othman@gmail.com>
2024-03-20 13:26:53 +0100

    Mise à jour de l'affichage des cases pour s'adapter au type de case

--- src/affichage/AffichageTerminal.java
+++ src/affichage/AffichageTerminal.java
@@ -1,17 +1,29 @@
 package affichage;
 
 import entites.Case;
+import entites.CaseArme;
+import utils.Effet;
 
 public class AffichageTerminal {
 	
 	public void afficherCase(Case caseAfficher) {
-		if ( caseAfficher != null) {
-		System.out.println("Num√©ro case = " + caseAfficher.getNumero());
-		System.out.println("Effet case = " + caseAfficher.getEffet());
-		}
-		else 
+		if (caseAfficher == null)
 			System.out.println("La case √† afficher est null ");
+		else if ( caseAfficher.getEffet() == Effet.ARME) {
+			System.out.println("Num√©ro case = " + caseAfficher.getNumero());
+			afficherArme((CaseArme) caseAfficher);
+		}
+		else if ( caseAfficher.getEffet() == Effet.RHUM) {
+			System.out.println("Num√©ro case = " + caseAfficher.getNumero());
+			System.out.println("Sur la case il y ‡ du rhum et un vrai pirate ne dis pas non ‡ du rhum.");
+		}
+
 	}
+	
+	public void afficherArme(CaseArme caseArme) {
+		System.out.println("Sur la case il y ‡ " + caseArme.getArme());
+	}
+	
 	public void afficherPosition(int positionAfficher) {
 		System.out.println("Num√©ro case = " + positionAfficher);
 		
