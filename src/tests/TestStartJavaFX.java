ba8aaca4e4c51c1cc82652415b286fa2205beadb
Belgnaoui Othman <belgnaoui.othman@gmail.com>
2024-04-02 02:00:53 +0200

    Test jeu complet dans TestStartJavaFX. Il manque l'affichage des effets de cases et des pirates sur le plateau

--- src/tests/TestStartJavaFX.java
+++ src/tests/TestStartJavaFX.java
@@ -1,5 +1,25 @@
 package tests;
 
-public class TestStartJavaFX {
+import affichage.AffichageJavaFX;
+import entites.Jeu;
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class TestStartJavaFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args); // Launch the JavaFX application
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a grid pane to display the plateau
+        GridPane plateauGrid = new GridPane();
+        
+        AffichageJavaFX affichage = new AffichageJavaFX(plateauGrid);
+        Jeu jeu = new Jeu(affichage);
+
+        jeu.start();
+    }
 }
